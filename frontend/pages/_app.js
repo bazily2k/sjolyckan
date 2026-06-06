b6e2539e255f4f9c146b6dd265a01f47171179fc
bazily2k <116429407+bazily2k@users.noreply.github.com>
2026-06-06 11:39:02 +0200

    Update _app.js
    feat(cms): admin-managed rooms (drop Airbnb fallback) + WYSIWYG hero headings; add favicon

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import { appWithTranslation } from 'next-i18next';
 import { AuthProvider } from '../lib/auth';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
